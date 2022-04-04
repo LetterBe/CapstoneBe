ed02895524ef5378a011b4f0a42e3c7686abf83b
Bárbara <barbarap@tutanota.com>
2022-04-04 12:33:24 +0200

    create methode in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import TodoForm from "./todoForm/TodoForm";
 
 function App() {
 
-    const [greeting, setGreeting] = useState('')
-
-    useEffect(() => {
-        fetch('/api/greeting', {
-            method: 'GET',
-            headers: {
-                'Accept': 'text/plain'
-            }
-        })
-            .then(response => response.text())
-            .then(text => setGreeting(text))
-            .catch(err => setGreeting('Da ist etwas schief gelaufen'));
-    }, []);
-
     return (
         <div>
-            {greeting}
+            <TodoForm />
         </div>
     );
 }
