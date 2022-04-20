acf2ea1d6def160b6f702a98fcb2b65ca7257dcb
Bárbara <barbarap@tutanota.com>
2022-04-20 11:29:54 +0200

    success with useCallback

--- frontend/src/todos/TodoPage.tsx
+++ frontend/src/todos/TodoPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {TodoDTO} from "./TodoDTOModel";
 import TodoEdit from "./TodoEdit";
 import '../css/TodoPage.css';
@@ -12,7 +12,12 @@ export default function TodoPage() {
     const [selectedTodo, setSelectedTodo] = useState({} as TodoDTO)
     const [score, setScore] = useState(0)
 
-    const fetchAll = () => {
+    const scoreAdd = useCallback( () => {
+        setScore(0)
+        todos.forEach((todo: TodoDTO) => (setScore(score + todo.score)))
+    }, [score, todos])
+
+    const fetchAll = useCallback (() => {
         const token = localStorage.getItem('token')
         fetch(`${process.env.REACT_APP_BASE_URL}/api/todos`, {
             headers: {
@@ -25,29 +30,25 @@ export default function TodoPage() {
                 scoreAdd()
                 setSelectedTodo({} as TodoDTO)
             })
-    }
+    }, [scoreAdd])
 
-    const scoreAdd = () => {
-        setScore(0)
-        todos.forEach((todo: TodoDTO) => (setScore(score + todo.score)))
-    }
 
     useEffect(() => {
         fetchAll()
-    }, []);
+    }, [fetchAll]);
 
     return (
         <>
             <div className='app2'>
-                <div>
-                    {score}
-                </div>
                 <Text size='2xl'
                       message={`${localStorage.getItem('username') === null ? '' : 'Hi,  ' + localStorage.getItem('username') + ', nice to have you here!'}`}/>
                 <Text message='Here you create new Tasks, edit them and
                  when you are done, check it before deleting, so you
                 get your score higher'/>
                 <TodoEdit onTodoChange={fetchAll} todoToChange={selectedTodo}/>
+            </div>
+            <div className= 'score'>
+                <Text message={`Score: ${score}`} />
             </div>
             <div className='postItContainer'>
                 {todos.length > 0 && todos.map((todo) => <TodoItem key={todo.id} todoItem={todo}
