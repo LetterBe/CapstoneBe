e65f548a09586df1595e074bcd495b1428dcefa6
Bárbara <barbarap@tutanota.com>
2022-04-21 11:42:41 +0200

    setup flapdoodle und begin It

--- backend/src/test/java/com/example/demo/IntegrationTest.java
+++ backend/src/test/java/com/example/demo/IntegrationTest.java
@@ -2,11 +2,14 @@ package com.example.demo;
 
 
 import com.example.demo.login.LoginData;
-import com.example.demo.todo.model.Todo;
+
+import com.example.demo.todo.TodoCategory;
+import com.example.demo.todo.TodoStatus;
 import com.example.demo.todo.modelDTO.TodoDTO;
 import com.example.demo.user.UserDocument;
+
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Order;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,35 +23,52 @@ public class IntegrationTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    // Test-Object
 
-    final private Todo todoItem1 = new Todo();
-    final private Todo todoItem2 = new Todo();
+    @Test
+    void integrationsTest () {
+        TodoDTO todo = new TodoDTO("3", "mustermensch@web.de", "einkaufen", "milk, bread, fruits", TodoCategory.Home, false, 0);
 
 
-    /*@Test
-    @Order(1)
-    void shouldCreateNewTodoItem () {
-        ResponseEntity<UserDocument> createUserResponse = restTemplate.postForEntity("api/login/register", new UserDocument("3","mustermensch@web.de", "3456b"),UserDocument.class );
+
+        ResponseEntity<UserDocument> createUserResponse = restTemplate.postForEntity("/api/login/register", new UserDocument("3","mustermensch@web.de", "3456b"),UserDocument.class );
         Assertions.assertThat(createUserResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
 
         ResponseEntity<String> loginResponse = restTemplate.postForEntity("/api/login", new LoginData("mustermensch@web.de", "3456b"), String.class);
         Assertions.assertThat(loginResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
         Assertions.assertThat(loginResponse.getBody()).isNotBlank();
+        String token = loginResponse.getBody();
+        HttpHeaders baererHeader = new HttpHeaders();
+        baererHeader.setBearerAuth(token);
 
-        ResponseEntity<TodoDTO> createTodoItem = restTemplate.exchange(
+        ResponseEntity<TodoDTO> createTodo = restTemplate.exchange(
                 "/api/todos",
                 HttpMethod.POST,
-                new HttpEntity<>(todoItem1, createHeaders(loginResponse.getBody()) )
-        )
+                new HttpEntity<>(todo, baererHeader),
+                TodoDTO.class
+        );
+
+
+        TodoDTO createdTodo = createTodo.getBody();
+
+        Assertions.assertThat(createTodo.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        Assertions.assertThat(createdTodo.toTodo()).isEqualTo(todo.toTodo());
+
+        ResponseEntity<TodoDTO> updateTodo = restTemplate.exchange(
+                "/api/todos" + createdTodo.getId(),
+                HttpMethod.PUT,
+                new HttpEntity<>(createdTodo, baererHeader),
+                TodoDTO.class
+        );
+
+        TodoDTO updatedTodo = updateTodo.getBody();
+
+        Assertions.assertThat(updateTodo.getStatusCode()).isEqualTo(HttpStatus.OK)
+        Assertions.assertThat(updatedTodo.toTodo()).isEqualTo(createdTodo.toTodo());
+
+
     }
 
-    private HttpHeaders createHeaders(String token){
-        String authHeader = "Bearer " + token;
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", authHeader);
 
-        return headers;
-    }*/
+
 
 }
