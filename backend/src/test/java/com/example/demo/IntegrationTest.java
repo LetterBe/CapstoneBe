c720c067dffbb790d2764e70b1cfddd574773377
Bárbara <barbarap@tutanota.com>
2022-04-21 12:38:37 +0200

    test: create use, create todo, update todo, check todo

--- backend/src/test/java/com/example/demo/IntegrationTest.java
+++ backend/src/test/java/com/example/demo/IntegrationTest.java
@@ -28,15 +28,16 @@ public class IntegrationTest {
     void integrationsTest () {
         TodoDTO todo = new TodoDTO("3", "mustermensch@web.de", "einkaufen", "milk, bread, fruits", TodoCategory.Home, false, 0);
 
-
-
         ResponseEntity<UserDocument> createUserResponse = restTemplate.postForEntity("/api/login/register", new UserDocument("3","mustermensch@web.de", "3456b"),UserDocument.class );
         Assertions.assertThat(createUserResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
 
         ResponseEntity<String> loginResponse = restTemplate.postForEntity("/api/login", new LoginData("mustermensch@web.de", "3456b"), String.class);
+
         Assertions.assertThat(loginResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
         Assertions.assertThat(loginResponse.getBody()).isNotBlank();
+
         String token = loginResponse.getBody();
+
         HttpHeaders baererHeader = new HttpHeaders();
         baererHeader.setBearerAuth(token);
 
@@ -51,19 +52,32 @@ public class IntegrationTest {
         TodoDTO createdTodo = createTodo.getBody();
 
         Assertions.assertThat(createTodo.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        Assertions.assertThat(createdTodo.toTodo()).isEqualTo(todo.toTodo());
+        Assertions.assertThat(createdTodo).isEqualTo(todo);
+
+        TodoDTO updatedTodo = new TodoDTO("3", "mustermensch@web.de", "einkaufen", "milk,rice", TodoCategory.Home, false, 0);
 
         ResponseEntity<TodoDTO> updateTodo = restTemplate.exchange(
-                "/api/todos" + createdTodo.getId(),
+                "/api/todos/" + createdTodo.getId(),
                 HttpMethod.PUT,
-                new HttpEntity<>(createdTodo, baererHeader),
+                new HttpEntity<>(updatedTodo, baererHeader),
                 TodoDTO.class
         );
 
-        TodoDTO updatedTodo = updateTodo.getBody();
 
-        Assertions.assertThat(updateTodo.getStatusCode()).isEqualTo(HttpStatus.OK)
-        Assertions.assertThat(updatedTodo.toTodo()).isEqualTo(createdTodo.toTodo());
+        Assertions.assertThat(updateTodo.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(updateTodo.getBody().toTodo()).isEqualTo(updatedTodo.toTodo());
+
+        TodoDTO doneTodo = new TodoDTO("3", "mustermensch@web.de", "einkaufen", "milk,rice", TodoCategory.Home, true, 0);
+
+        ResponseEntity<TodoDTO> setTodoDone = restTemplate.exchange(
+                "/api/todos/" + doneTodo.getId(),
+                HttpMethod.PUT,
+                new HttpEntity<>(doneTodo, baererHeader),
+                TodoDTO.class
+        );
+
+        Assertions.assertThat(setTodoDone.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(setTodoDone.getBody().toTodo().getStatus()).isEqualTo(TodoStatus.Done);
 
 
     }
